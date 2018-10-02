cb3205a5570d213d308a1c8508805426984c46b0
Bryan Phelps <bryphe@outlook.com>
2018-10-01 18:58:53 -0700

    Tweaks

--- esy-configure-windows.sh
+++ esy-configure-windows.sh
@@ -1,6 +1,10 @@
-mkdirp _build
+mkdir -p _build
 cd _build
 
-echo $curr__build
+echo $PATH
 
-cmake -G "Unix Makefiles" ../ -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_AR=/usr/bin/x86_64-w64-mingw32-ar -DCMAKE_LINKER=/usr/bin/x86_64-w64-mingw32-ld -DCMAKE_NM=/usr/bin/x86_64-w64-mingw32-nm -DCMAKE_OBJCOPY=/usr/bin/x86_64-w64-mingw32-objcopy -DCMAKE_OBJDUMP=/usr/bin/x86_64-w64-mingw32-objdump -DCMAKE_RANLIB=/usr/bin/x86_64-w64-mingw32-ranlib -DCMAKE_RC_COMPILER=/usr/bin/x86_64-w64-mingw32-windres -DCMAKE_INSTALL_PREFIX=$curr__build
+/usr/bin/x86_64-w64-mingw32-ar --version
+
+cmake -G "Unix Makefiles" ../ -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_LINKER=/usr/bin/x86_64-w64-mingw32-ld -DCMAKE_AR=/usr/bin/x86_64-w64-mingw32-ar
+
+make VERBOSE=1
