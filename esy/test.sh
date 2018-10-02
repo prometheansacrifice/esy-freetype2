691efee99bea13a1e9bb061c2d09546741357a49
Bryan Phelps <bryphe@outlook.com>
2018-10-02 10:49:13 -0700

    Build in sandbox

--- esy/test.sh
+++ esy/test.sh
@@ -1,11 +1,17 @@
 pwd
-cd esy
+cd _build
 
 INCLUDE=$cur__install/include/freetype2
+
+if which x86_64-w64-mingw32-gcc; then
+    CC=x86_64-w64-mingw32-gcc
+fi
+
+echo "Using compiler: $CC"
 
 echo "include..."
 ls -a $INCLUDE
 echo "lib.."
 ls -a $cur__lib
-x86_64-w64-mingw32-gcc ./test.c -o ./test -I$INCLUDE -L$cur__lib -lfreetype
+$CC ./../esy/test.c -o ./test -I$INCLUDE -L$cur__lib -lfreetype
 ./test
