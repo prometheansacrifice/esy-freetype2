b01bf9a7543b5abdb506525d11fba51e4c68e4d7
Bryan Phelps <bryphe@outlook.com>
2018-10-02 15:41:37 -0700

    Put condition around additional libs

--- esy/test.sh
+++ esy/test.sh
@@ -5,8 +5,10 @@ INCLUDE=$cur__install/include/freetype2
 
 if which x86_64-w64-mingw32-gcc; then
     CC=x86_64-w64-mingw32-gcc
+    CCLIB=
 else
     CC=gcc
+    CCLIB=-lbz2 -lpng -lz
 fi
 
 echo "Using compiler: $CC"
@@ -15,5 +17,5 @@ echo "include..."
 ls -a $INCLUDE
 echo "lib.."
 ls -a $cur__lib
-$CC ./../esy/test.c -o ./test -I$INCLUDE -L$cur__lib -lfreetype -L/usr/lib -L/usr/local/lib -lbz2 -lpng -lz
+$CC ./../esy/test.c -o ./test -I$INCLUDE -L$cur__lib -lfreetype -L/usr/lib -L/usr/local/lib $CCLIB
 ./test
